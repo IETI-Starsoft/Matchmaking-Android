19c852bdfcd29bba5408a763a38e43974de07fff
Julian Benitez <julian.benitez@mail.escuelaing.edu.co>
2020-05-11 00:07:55 -0500

    :construction: Profile view and fragments

---
+++ app/src/main/java/edu/escuelaing/matchmaking/ui/menu/viewmodel/PerfilViewModel.java
@@ -0,0 +1,20 @@
+package edu.escuelaing.matchmaking.ui.menu.viewmodel;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class PerfilViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public PerfilViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is Perfil fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+
+}
