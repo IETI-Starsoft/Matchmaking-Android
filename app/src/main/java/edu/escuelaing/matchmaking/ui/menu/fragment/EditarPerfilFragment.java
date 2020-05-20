cf92de332e3041793b704cf581b63645d0c2b5d4
Julian Benitez <julian.benitez@mail.escuelaing.edu.co>
2020-05-20 16:20:04 -0500

    :construction: Edit profile fragment

--- app/src/main/java/edu/escuelaing/matchmaking/ui/menu/fragment/EditarPerfilFragment.java
+++ app/src/main/java/edu/escuelaing/matchmaking/ui/menu/fragment/EditarPerfilFragment.java
@@ -4,33 +4,99 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatEditText;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import edu.escuelaing.matchmaking.R;
+import edu.escuelaing.matchmaking.network.RetrofitNetwork;
+import edu.escuelaing.matchmaking.pojo.User;
+import edu.escuelaing.matchmaking.storage.Storage;
 import edu.escuelaing.matchmaking.ui.menu.viewmodel.EditarPerfilViewModel;
+import edu.escuelaing.matchmaking.ui.utils.StringUtils;
+import retrofit2.Call;
+import retrofit2.Response;
 
 public class EditarPerfilFragment extends Fragment{
 
     private EditarPerfilViewModel editarPerfilViewModel;
 
+    private final ExecutorService executorService = Executors.newFixedThreadPool( 1 );
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        Storage storage = new Storage(getContext());
+        final RetrofitNetwork retrofitNetwork = new RetrofitNetwork(storage.getToken());
         editarPerfilViewModel =
                 ViewModelProviders.of(this).get(EditarPerfilViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_editar_perfil, container, false);
-        final TextView textView = root.findViewById(R.id.text_editar_perfil);
-        editarPerfilViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        editarPerfilViewModel.setRetrofitNetwork(retrofitNetwork);
+        editarPerfilViewModel.setUserEmail(storage.getEmail());
+        final View root = inflater.inflate(R.layout.fragment_editar_perfil, container, false);
+        final TextInputEditText userNameText = root.findViewById(R.id.editName);
+        final AppCompatEditText userBioText = root.findViewById(R.id.editBio);
+        Button updateButton = root.findViewById(R.id.update_button);
+
+        editarPerfilViewModel.getUserNameText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onChanged(String s) {
+                userNameText.setHint(s);
             }
         });
+
+        editarPerfilViewModel.getUserBioText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+               userBioText.setHint(s);
+            }
+        });
+
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final String name = userNameText.getText().toString();
+                final String bio = userBioText.getText().toString();
+                if (name.isEmpty() && bio.isEmpty() ) {
+                    userNameText.setError(getString(R.string.campo_vacio));
+                    userBioText.setError(getString(R.string.campo_vacio));
+                }
+                else {
+                    executorService.execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            String info = "¡Datos actualizados!";
+                            Snackbar snackBar = Snackbar.make(getActivity().findViewById(R.id.drawer_layout),
+                                    info, Snackbar.LENGTH_LONG);
+                            try {
+                                editarPerfilViewModel.updateUserData(name, bio);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                                snackBar.setText("Error actualizando datos.");
+                                View snackbarView = snackBar.getView();
+                                snackbarView.setBackgroundColor(getResources().getColor(R.color.errorSnackbar));
+                            }
+                            snackBar.show();
+                        }
+                    });
+
+                }
+            }
+        });
+
         return root;
     }
+
+
 }
--- app/src/main/java/edu/escuelaing/matchmaking/ui/menu/viewmodel/EditarPerfilViewModel.java
+++ app/src/main/java/edu/escuelaing/matchmaking/ui/menu/viewmodel/EditarPerfilViewModel.java
@@ -4,16 +4,78 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class EditarPerfilViewModel extends ViewModel{
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-    private MutableLiveData<String> mText;
+import edu.escuelaing.matchmaking.network.RetrofitNetwork;
+import edu.escuelaing.matchmaking.pojo.User;
+import retrofit2.Call;
+import retrofit2.Response;
+
+public class EditarPerfilViewModel extends ViewModel {
+
+    private MutableLiveData<String> userName;
+
+    private MutableLiveData<String> userBio;
+
+    private RetrofitNetwork retrofitNetwork;
+
+    private String userEmail;
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(1);
 
     public EditarPerfilViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is editar perfil fragment");
+        userName = new MutableLiveData<>();
+        userBio = new MutableLiveData<>();
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<String> getUserNameText() {
+        return userName;
     }
+
+    public LiveData<String> getUserBioText() {
+        return userBio;
+    }
+
+    public void setRetrofitNetwork(RetrofitNetwork retrofitNetwork) {
+        this.retrofitNetwork = retrofitNetwork;
+        update();
+    }
+
+    private void update() {
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                Call<User> call = retrofitNetwork.getUserService().getUser(userEmail);
+                try {
+                    Response<User> response = call.execute();
+                    User user = response.body();
+                    userName.postValue(user.getFirstName() + " " + user.getLastName());
+                    userBio.postValue("Bio"); // CHANGE
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void updateUserData(String name, String bio) throws IOException {
+        Call<User> callUser = retrofitNetwork.getUserService().getUser(userEmail);
+        User user = callUser.execute().body();
+        if (!name.isEmpty()){
+            user.setFirstName(name);
+            user.setLastName("");
+        }
+        if (!bio.isEmpty()){
+            //user.setBio
+        }
+        Call<User> callUpdate = retrofitNetwork.getUserService().updateUser(user);
+        Response<User> response = callUpdate.execute();
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
 }
