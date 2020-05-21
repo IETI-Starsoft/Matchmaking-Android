7a6e9749af569d9f916d88e9a75a6fb6bca05f1b
diego2097 <ye.yo14@hotmail.com>
2020-05-20 19:56:19 -0500

    :construction: initial view

--- app/src/main/java/edu/escuelaing/matchmaking/ui/menu/fragment/CrearMatchFragment.java
+++ app/src/main/java/edu/escuelaing/matchmaking/ui/menu/fragment/CrearMatchFragment.java
@@ -1,36 +1,77 @@
 package edu.escuelaing.matchmaking.ui.menu.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.core.util.Pair;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProviders;
+
+import com.google.android.material.datepicker.MaterialDatePicker;
 
 import edu.escuelaing.matchmaking.R;
 import edu.escuelaing.matchmaking.ui.menu.viewmodel.CrearMatchViewModel;
 
-public class CrearMatchFragment extends Fragment{
+public class CrearMatchFragment extends Fragment  {
 
     private CrearMatchViewModel crearMatchViewModel;
-
+    private View root;
+    private Context mContext;
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+                             final ViewGroup container, Bundle savedInstanceState) {
         crearMatchViewModel =
                 ViewModelProviders.of(this).get(CrearMatchViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_crear_match, container, false);
-        final TextView textView = root.findViewById(R.id.text_crear_match);
-        crearMatchViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+
+        root = inflater.inflate(R.layout.fragment_crear_match, container, false);
+        MaterialDatePicker.Builder<Pair<Long, Long>> builder = MaterialDatePicker.Builder.dateRangePicker();
+
+        root.findViewById(R.id.suscription).setVisibility(View.INVISIBLE);
+        //checkbox suscripcion
+        final CheckBox checkbox = (CheckBox) root.findViewById(R.id.checkbox);
+        checkbox.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+                boolean checked = checkbox.isChecked();
+                if (checked)
+                    showAmount(root);
+                else
+                    hideAmount(root);
             }
         });
+        final Button confirmar = (Button) root.findViewById(R.id.confirmar);
+        confirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
         return root;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
+
+    public void showAmount(View view){
+        View amount = view.findViewById(R.id.suscription);
+        amount.setVisibility(View.VISIBLE);
+    }
+
+    public void hideAmount(View view){
+        View amount = view.findViewById(R.id.suscription);
+        amount.setVisibility(View.INVISIBLE);
     }
 }
--- app/src/main/java/edu/escuelaing/matchmaking/ui/menu/viewmodel/CrearMatchViewModel.java
+++ app/src/main/java/edu/escuelaing/matchmaking/ui/menu/viewmodel/CrearMatchViewModel.java
@@ -1,19 +1,92 @@
 package edu.escuelaing.matchmaking.ui.menu.viewmodel;
 
+import android.view.View;
+import android.widget.CheckBox;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.Date;
+
 public class CrearMatchViewModel extends ViewModel{
 
-    private MutableLiveData<String> mText;
+    private String retador;
+    private String activity;
+    private String location;
+    private String description;
+    private String date;
+    private String time;
+    private Integer bet;
+    private boolean participants;
+
 
     public CrearMatchViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is crear match fragment");
     }
 
-    public LiveData<String> getText() {
-        return mText;
+
+    public boolean isParticipants() {
+        return participants;
     }
+
+    public void setParticipants(boolean participants) {
+        this.participants = participants;
+    }
+
+    public String getRetador() {
+        return retador;
+    }
+
+    public void setRetador(String retador) {
+        this.retador = retador;
+    }
+
+    public String getActivity() {
+        return activity;
+    }
+
+    public void setActivity(String activity) {
+        this.activity = activity;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public Integer getBet() {
+        return bet;
+    }
+
+    public void setBet(Integer bet) {
+        this.bet = bet;
+    }
+
 }
